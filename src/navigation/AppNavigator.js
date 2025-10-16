0cf4b63cb8932ab5694e1276d0036396ccb11a80
BruGeth <g15bru03@hotmail.com>
2025-10-16 17:47:41 -0500

    feat: actualizar AppNavigator para establecer Main como ruta inicial

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AuthScreen from "../screens/AuthScreen";
+import MainTabs from "./MainTabs";
 
 const Stack = createNativeStackNavigator();
 
@@ -8,10 +9,11 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Auth"
+        initialRouteName="Main"
         screenOptions={{ headerShown: false }}
       >
-        <Stack.Screen name="Auth" component={AuthScreen} />
+         <Stack.Screen name="Auth" component={AuthScreen} />
+         <Stack.Screen name="Main" component={MainTabs} />
         {/* agrega más pantallas aquí */}
       </Stack.Navigator>
     </NavigationContainer>
